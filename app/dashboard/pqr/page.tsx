f7cc415e5d1b0faa79af70259ec62013db5f3f20
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2024-12-14 12:42:07 -0500

    Add PQR charts and enhance dashboard UI

--- app/dashboard/pqr/page.tsx
+++ app/dashboard/pqr/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import PqrVsDepartmentChart from "@/components/charts/pqr/pqr-vs-deparment";
+import { PqrVsTimeChart } from "@/components/charts/pqr/pqr-vs-time";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,6 +11,7 @@ import {
   CardHeader,
   CardDescription,
 } from "@/components/ui/card";
+import { ChartContainer, ChartTooltip } from "@/components/ui/chart";
 import {
   Table,
   TableHeader,
@@ -24,6 +29,15 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { getPQRS } from "@/services/api/pqr.service";
 import { Prisma } from "@prisma/client";
 import { EyeIcon } from "lucide-react";
+import {
+  BarChart,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Bar,
+} from "recharts";
 
 const pqrs = await getPQRS();
 
@@ -46,69 +60,88 @@ export default function PQRPage() {
     }
   }
 
+  function getPqrsByDate() {
+    const pqrsByDate = pqrs.reduce((acc: any, pqr: any) => {
+      const date = new Date(pqr.createdAt).toISOString().split("T")[0];
+      acc[date] = (acc[date] || 0) + 1;
+      return acc;
+    }, {});
+
+    return Object.entries(pqrsByDate).map(([date, count]) => ({
+      name: date,
+      count,
+    }));
+  }
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>PQRS</CardTitle>
-        <CardDescription>
-          Gestiona las PQRS creadas por los usuarios
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>Tipo</TableHead>
-              <TableHead>Asunto</TableHead>
-              <TableHead>Descripción</TableHead>
-              <TableHead>Departamento</TableHead>
-              <TableHead>Creador</TableHead>
-              <TableHead>Tiempo para responder</TableHead>
-              <TableHead>Acciones</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {pqrs.map(
-              (
-                pqr: Prisma.PQRSGetPayload<{
-                  include: { department: true; creator: true };
-                }>
-              ) => (
-                <TableRow key={pqr.id}>
-                  <TableCell>{pqr.id.slice(0, 6)}</TableCell>
-                  <TableCell>{pqr.type}</TableCell>
-                  <TableCell>{pqr.subject}</TableCell>
-                  <TableCell>{pqr.description}</TableCell>
-                  <TableCell>{pqr.department?.name}</TableCell>
-                  <TableCell>
-                    {pqr.creator?.firstName} {pqr.creator?.lastName}
-                  </TableCell>
-                  <TableCell className="flex justify-center">
-                    {getRemainingTimeBadge(pqr.createdAt)}
-                  </TableCell>
-                  <TableCell>
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <a href={`/dashboard/pqr/${pqr.id}`}>
-                            <Button variant="outline" size="icon">
-                              <EyeIcon className="w-4 h-4" />
-                            </Button>
-                          </a>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Ver PQRS</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </TableCell>
-                </TableRow>
-              )
-            )}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row gap-4 w-full">
+        <PqrVsTimeChart pqrs={pqrs} />
+        <PqrVsDepartmentChart pqrs={pqrs} />
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>PQRS</CardTitle>
+          <CardDescription>
+            Gestiona las PQRS creadas por los usuarios
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>Tipo</TableHead>
+                <TableHead>Asunto</TableHead>
+                <TableHead>Descripción</TableHead>
+                <TableHead>Departamento</TableHead>
+                <TableHead>Creador</TableHead>
+                <TableHead>Tiempo para responder</TableHead>
+                <TableHead>Acciones</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {pqrs.map(
+                (
+                  pqr: Prisma.PQRSGetPayload<{
+                    include: { department: true; creator: true };
+                  }>
+                ) => (
+                  <TableRow key={pqr.id}>
+                    <TableCell>{pqr.id.slice(0, 6)}</TableCell>
+                    <TableCell>{pqr.type}</TableCell>
+                    <TableCell>{pqr.subject}</TableCell>
+                    <TableCell>{pqr.description}</TableCell>
+                    <TableCell>{pqr.department?.name}</TableCell>
+                    <TableCell>
+                      {pqr.creator?.firstName} {pqr.creator?.lastName}
+                    </TableCell>
+                    <TableCell className="flex justify-center">
+                      {getRemainingTimeBadge(pqr.createdAt)}
+                    </TableCell>
+                    <TableCell>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <a href={`/dashboard/pqr/${pqr.id}`}>
+                              <Button variant="outline" size="icon">
+                                <EyeIcon className="w-4 h-4" />
+                              </Button>
+                            </a>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Ver PQRS</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
---
+++ components/charts/pqr/pqr-vs-deparment.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { ChartContainer, ChartTooltip } from "@/components/ui/chart";
+import { BarChart, CartesianGrid, XAxis, YAxis, Bar } from "recharts";
+
+export default function PqrVsDepartmentChart({ pqrs }: { pqrs: any }) {
+  function getPqrsByDepartment() {
+    const pqrsByDepartment = pqrs.reduce((acc: any, pqr: any) => {
+      const department = pqr.department?.name;
+      acc[department] = (acc[department] || 0) + 1;
+      return acc;
+    }, {});
+
+    return Object.entries(pqrsByDepartment).map(([department, count]) => ({
+      name: department,
+      count,
+    }));
+  }
+  return (
+    <Card className="w-1/2">
+      <CardHeader>
+        <CardTitle>PQRS por departamento</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={{}}>
+          <BarChart data={getPqrsByDepartment()} accessibilityLayer>
+            <CartesianGrid vertical={false} />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <ChartTooltip />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
