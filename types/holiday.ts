14830d715cfc4f1d510e188cae420ad69950a50f
Dania Sarmiento <dania.v.sarmiento@gmail.com>
2025-07-25 17:34:06 -0500

    feat: Add Colombian holidays constants and update business day calculations

---
+++ types/holiday.ts
@@ -0,0 +1,8 @@
+export type Holiday = {
+    date: string;
+    start: string;
+    end: string;
+    name: string;
+    type: string;
+    rule: string;
+};
--- utils/dateHelpers.ts
+++ utils/dateHelpers.ts
@@ -1,26 +1,47 @@
+import { COLOMBIAN_HOLIDAYS } from "@/constants/holidays";
+import { Holiday } from "@/types/holiday";
+
+function isHoliday(date: Date, holidays: Holiday[]): boolean {
+    const dateStr = date.toISOString().split('T')[0];
+    return holidays.some(holiday => {
+        const holidayDate = new Date(holiday.date);
+        const holidayDateStr = holidayDate.toISOString().split('T')[0];
+        return holidayDateStr === dateStr;
+    });
+}
+
+function isBusinessDay(date: Date, holidays: Holiday[]): boolean {
+    const dayOfWeek = date.getDay();
+    const isWeekday = dayOfWeek !== 0 && dayOfWeek !== 6;
+    return isWeekday && !isHoliday(date, holidays);
+}
+
 export function calculateBusinessDaysExceeded(createdAt: Date, daysThreshold = 15): number {
-  const createdDate = new Date(createdAt);
-  const currentDate = new Date();
-  let businessDaysCount = 0;
-  let exceededDays = 0;
-  
-  const tempDate = new Date(createdDate);
+    const createdDate = new Date(createdAt);
+    const currentDate = new Date();
+    let businessDaysCount = 0;
+    let exceededDays = 0;
 
-  while (businessDaysCount < daysThreshold && tempDate <= currentDate) {
-    tempDate.setDate(tempDate.getDate() + 1);
-    if (tempDate.getDay() !== 0 && tempDate.getDay() !== 6) {
-      businessDaysCount++;
+    createdDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(0, 0, 0, 0);
+    
+    const tempDate = new Date(createdDate);
+
+    while (businessDaysCount < daysThreshold && tempDate <= currentDate) {
+        if (isBusinessDay(tempDate, COLOMBIAN_HOLIDAYS)) {
+            businessDaysCount++;
+        }
+        tempDate.setDate(tempDate.getDate() + 1);
     }
-  }
 
-  if (businessDaysCount < daysThreshold) return 0;
+    if (businessDaysCount < daysThreshold) return 0;
 
-  while (tempDate <= currentDate) {
-    if (tempDate.getDay() !== 0 && tempDate.getDay() !== 6) {
-      exceededDays++;
+    while (tempDate <= currentDate) {
+        if (isBusinessDay(tempDate, COLOMBIAN_HOLIDAYS)) {
+            exceededDays++;
+        }
+        tempDate.setDate(tempDate.getDate() + 1);
     }
-    tempDate.setDate(tempDate.getDate() + 1);
-  }
 
-  return exceededDays;
+    return exceededDays;
 }
