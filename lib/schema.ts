f0be77d8d8edef0d67ef47228c8f8ad3eba365c0
Dania Sarmiento <dania.v.sarmiento@gmail.com>
2025-08-07 14:42:23 -0500

    Remove unused signUp action and schema definitions

--- lib/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const schema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, "La contraseña debe tener al menos 1 carácter"),
-});
-
-export { schema }
