ea6a57a3796b66a97e54647442e27cd744bd5ad7
Dania Sarmiento <dania.v.sarmiento@gmail.com>
2025-09-28 14:41:04 -0500

    Refactor notification types to improve structure and clarity

--- types/notification.ts
+++ types/notification.ts
@@ -1,19 +1,53 @@
+export type NotificationType =
+  | "follow"
+  | "mention"
+  | "like"
+  | "comment"
+  | "lawyer_request_accepted"
+  | "lawyer_request_rejected"
+  | "new_lawyer_request"
+  | "pqrsd_time_expired"
+  ;
+
+export type NotificationData =
+  | {
+    type: "follow";
+    followerId: string;
+    followerName: string;
+    followerImage?: string;
+  }
+  | {
+    type: "like" | "comment";
+    pqrId: string;
+    userId: string;
+    userName: string;
+    userImage?: string;
+  }
+  | {
+    type: "lawyer_request_accepted" | "lawyer_request_rejected";
+    requestId: string;
+    lawyerName: string;
+    rejectionMessage?: string;
+  }
+  | {
+    type: "new_lawyer_request";
+    requestId: string;
+    clientName: string;
+    pqrSubject?: string;
+  }
+  | {
+    type: "pqrsd_time_expired";
+    pqrId: string;
+    pqrSubject: string;
+  };
+
 
 export type Notification = {
   id: string;
-  type: "follow" | "mention" | "like" | "comment" | "lawyer_request_accepted" | "lawyer_request_rejected" | "new_lawyer_request";
+  userId: string;
+  type: NotificationType;
   message: string;
   read: boolean;
   createdAt: Date;
-  data?: {
-    followerId?: string;
-    followerName?: string;
-    followerImage?: string;
-    pqrId?: string;
-    requestId?: string;
-    lawyerName?: string;
-    rejectionMessage?: string;
-    clientName?: string;
-    pqrSubject?: string;
-  };
+  data: NotificationData;
 };
